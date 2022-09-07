8720d14c38aa6c0627b5db17bb1320e7a7322157
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2022-09-07 20:49:45 +0300

    first commit

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,18 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {auth} from '../utchat'
+
+const SignIn = () => {
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth ,provider)
+  }
+
+  return (
+    <div>
+      <button onClick={signInWithGoogle}>Sign In with Google</button>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/utchat.tsx
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth } from "firebase/auth";
+
+interface fireConfig {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId: string;
+  }
+
+
+const firebaseConfig: fireConfig  = {
+    apiKey: "AIzaSyBxCPV_OcRLt2pnPtC1p77l3i0PHcC6u_0",
+    authDomain: "utchat-91b17.firebaseapp.com",
+    projectId: "utchat-91b17",
+    storageBucket: "utchat-91b17.appspot.com",
+    messagingSenderId: "1000651706507",
+    appId: "1:1000651706507:web:ca51e3d1eb6d334c59848e",
+    measurementId: "G-DK08EV963B"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export {db, auth};
+
