df73f89ec749c0c52fabf7888508c1e75537fe72
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2022-09-22 09:19:24 +0300

    ui update

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,20 +5,25 @@ import {
   limit,
   onSnapshot,
   orderBy,
-  query
+  query,
 } from "firebase/firestore";
 import { useEffect, useRef, useState } from "react";
 import { auth, db } from "../utchat";
 import SendMessage from "./SendMessage";
 import SignOut from "./SignOut";
 import Message from "./Message";
-import styles from "../styles/Chat.module.css"
+import styles from "../styles/Chat.module.css";
+
 
 const Chat = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [messages, setMessages] = useState<DocumentData[]>([]);
-  
+
   useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
     const q = query(collection(db, "messages"), orderBy("time"), limit(50));
     const chatMessages: Unsubscribe = onSnapshot(q, (snapshot) => {
       let messages: DocumentData[] = [];
@@ -26,32 +31,36 @@ const Chat = () => {
         messages.push({ ...doc.data(), id: doc.id });
       });
       setMessages(messages);
-      setTimeout(() => ref.current?.scrollIntoView(), 0)
+      setTimeout(() => ref.current?.scrollIntoView(), 0);
     });
     return () => chatMessages();
-  }, []);
+  };
 
-console.log(messages)
+
   return (
     <div>
       <SignOut />
       <div className={styles.outer_chat}>
         <div className={styles.chat_box}>
-          {messages.map(({ id, text, photoURL, displayName, uid}) => (
-              <div> 
-                <div
-                  key={id}
-                  className={`${styles.message} ${
-                    uid === auth.currentUser?.uid ? styles.sent : styles.received
-                  }`}
-                > 
-                  <img className={styles.user_photo} src={photoURL} alt="profile" />
-                  <div className={styles.username}>{displayName}</div>
-                  <div className={styles.divider}>|</div>
-                  <Message text={text} uid={uid} id={id}/>
-                </div>
+          {messages.map(({ id, text, photoURL, displayName, uid }) => (
+            <div>
+              <div
+                key={id}
+                className={`${styles.message} ${
+                  uid === auth.currentUser?.uid ? styles.sent : styles.received
+                }`}
+              >
+                <img
+                  className={styles.user_photo}
+                  src={photoURL}
+                  alt="profile"
+                />
+                <div className={styles.username}>{displayName}</div>
+                <div className={styles.divider}>|</div>
+                <Message text={text} uid={uid} id={id} />
               </div>
-            ))}
+            </div>
+          ))}
           <div ref={ref}></div>
         </div>
       </div>
